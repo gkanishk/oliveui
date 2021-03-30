4275b7ae6f34382099a65365f23f7699c87288c5
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-03-30 08:55:21 +0530

    Added getting started

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,9 +1,15 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
 export default function Docs() {
     return (
-      <div>
-        <main>
-          docs
-        </main>
+      <div className="container">
+        <h1>Getting Started:</h1>
+        <p>To get started with Olive ui we need to link it's css inside head of your html file.</p>
+        <h3>CSS:</h3>
+        <SyntaxHighlighter language="html" style={materialLight}>
+        {`
+      <link href="https://cdn.jsdelivr.net/gh/gkanishk/oliveui@dev/styles/main.css" rel="stylesheet">
+        `}</SyntaxHighlighter>
       </div>
     )
   }
